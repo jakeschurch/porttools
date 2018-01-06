4f7992c420f36fd1556ea09526b071bac49f7d7c
Jakeschurch <12192670+jakeschurch@users.noreply.github.com>
2018-01-05 22:09:00 -0500

    code: Add portfolio file

--- portfolio/portfolio.go
+++ portfolio/portfolio.go
@@ -1,3 +1,10 @@
-// Package portfolioTools allows for storage of information regarding particular securities.
+// Package portfolio allows for storage of a portfolio struct.
 package portfolio
+
 import s "porttools/security"
+
+// Portfolio struct holds attributes relative to a financial security portfolio.
+type Portfolio struct {
+	Securities []*s.Security
+	Benchmark  *s.Security
+}
