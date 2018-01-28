d4273485e8d1563e85c29c4ea5cc88430a13d5d3
Jake Schurch <jschurch@bryant.edu>
2018-01-27 21:21:30 -0500

    refactor: Portfolio struct, add: MarketPortfolio struct (referenced in #3)

--- portfolio.go
+++ portfolio.go
@@ -1,7 +1,38 @@
 package porttools
 
-// Portfolio struct holds attributes relative to a financial security portfolio.
+import "errors"
+
+// MarketPortfolio struct holds instances of finacial stock securities.
+type MarketPortfolio struct {
+	Instruments map[string]*Security
+}
+
+// Portfolio struct holds instances of finacial stock holdings.
 type Portfolio struct {
-	Holdings  []*Holding
-	Benchmark *Security
+	Instruments map[string]*Holding
+	Orders      []*[]Order // reference to all holdings' order slices
+	Benchmark   *Security
+}
+
+// AddtoPortfolio adds holding instrument to Portfolio instance.
+func (p *Portfolio) AddtoPortfolio(h *Holding) (err error) {
+
+	if _, exists := p.Instruments[h.Ticker]; !exists {
+		p.Instruments[h.Ticker] = h
+		p.Orders = append(p.Orders, &h.Orders)
+
+		return nil
+	}
+	return errors.New("Holding already exists in Instruments map")
+
+}
+
+// AddtoPortfolio adds holding instrument to MarketPortfolio instance.
+func (p *MarketPortfolio) AddtoPortfolio(s *Security) (err error) {
+	if _, exists := p.Instruments[s.Ticker]; !exists {
+		p.Instruments[s.Ticker] = s
+		return nil
+	}
+	return errors.New("Security already exists in Instruments map")
+
 }
