2c01c91c78f1ca5f916a1e4e7692308f88a0541f
Jake Schurch <jschurch@bryant.edu>
2018-03-02 05:08:41 -0500

    Add Queue struct

---
+++ utils.go
@@ -0,0 +1,47 @@
+package porttools
+
+// Queue is an implementation of a FIFO container type.
+type Queue struct {
+	len    int
+	values []*Node
+}
+
+// Push stores value in the queue.
+func (queue *Queue) Push(node *Node) {
+	queue.len++
+	if queue.len-1 == 0 {
+		queue.values[0] = node
+		return
+	}
+	queue.values[queue.len] = node
+	return
+}
+
+// Pop removes and returns value from the queue.
+func (queue *Queue) Pop() (node *Node) {
+	queue.len--
+	if queue.len+1 == 0 {
+		return nil
+	}
+	node = queue.values[0]
+	queue.values = queue.values[0:]
+	return node
+}
+
+// NewQueue instantiates a new Queue.
+func NewQueue() *Queue {
+	q := Queue{
+		len: 0,
+	}
+	return &q
+}
+
+// Node represents data stored in a container.
+type Node struct {
+	data interface{}
+}
+
+// NewNode instantiates a new Node.
+func NewNode(data interface{}) *Node {
+	return &Node{data: data}
+}
