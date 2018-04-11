a5c6740b6d111f362f7016bbb360b47dedf35e45
Jake Schurch <jschurch@bryant.edu>
2018-04-11 00:08:29 -0400

    Refactor Instrument Structs
    Refactor Instrument structs in order to improve code quality and reusability.

--- instrument/instrument.go
+++ instrument/instrument.go
@@ -6,78 +6,31 @@ import (
 	"github.com/jakeschurch/porttools/utils"
 )
 
-// Financial is an interface that is used for types that are embedded,
-// as well as update its own metrics.
-type Financial interface {
-	Update(Quote) error
-	GetUnderlying() Financial
-	Volume(utils.Amount) utils.Amount
-	Ticker() string
-}
-
 // ------------------------------------------------------------------
 
 // Instrument is the base type of a financial widget.
-type Instrument struct {
-	ticker string
-	volume utils.Amount
-	Nticks uint
-}
-
-// NewInstrument instantities a new struct of type Instrument.
-func NewInstrument(ticker string, volume utils.Amount) *Instrument {
-	instrument := new(Instrument)
-	instrument.ticker = ticker
-	instrument.Nticks = 0
-	instrument.Volume(volume)
-	return instrument
-}
-
-// GetUnderlying returns nil.
-func (i Instrument) GetUnderlying() Financial {
-	return nil
-}
-
-// Volume can be used as a get/set method if 0 is delta.
-func (i Instrument) Volume(delta utils.Amount) utils.Amount {
-	return i.dxVolume(delta)
-}
-
-func (i *Instrument) dxVolume(delta utils.Amount) utils.Amount {
-	i.volume += delta
-	return i.volume
-}
-
-func (i Instrument) Ticker() string {
-	return i.ticker
-}
-
-// Update for an instrument is used to  implement Financial interface
-func (i Instrument) Update(q Quote) error {
-	return nil
+type Instrument interface {
+	Update(Quote)
 }
 
 // ------------------------------------------------------------------
 
 type Quote struct {
-	Instrument
-	Bid, Ask  utils.Amount
-	Timestamp time.Time
+	Ticker           string
+	Volume, Bid, Ask utils.Amount
+	Timestamp        time.Time
 }
 
-func NewQuote(bid, ask utils.Amount, ts time.Time, i Instrument) *Quote {
+func NewQuote(ticker string, volume, bid, ask utils.Amount, ts time.Time) *Quote {
 	return &Quote{
-		Instrument: i,
-		Bid:        bid, Ask: ask, Timestamp: ts,
+		Ticker: ticker, Volume: volume,
+		Bid: bid, Ask: ask, Timestamp: ts,
 	}
 }
 
-func (q Quote) GetUnderlying() Financial {
-	return q.Instrument
-}
-
-func (q Quote) Ticker() string {
-	return q.ticker
+func (q *Quote) Update(fed *Quote) {
+	q.Ask = fed.Ask
+	q.Bid = fed.Bid
 }
 
 // ------------------------------------------------------------------
@@ -95,86 +48,80 @@ func NewTick(bidSz, askSz utils.Amount, q *Quote) *Tick {
 	}
 }
 
-func (t *Tick) GetUnderlying() Financial {
-	return t.Quote
-}
-
-func (t *Tick) SetTicker(ticker string) {
-	t.ticker = ticker
+func (t *Tick) Update(fed *Quote) {
+	t.Update(fed)
 }
 
 // ------------------------------------------------------------------
 
-// Asset is tradeable instrument type.
-type Asset struct {
-	*Quote
+// AssetSumm reflects an Insturment's trading metrics.
+type AssetSumm struct {
+	nTicks                      uint
+	TotalVolume, AvgBid, AvgAsk utils.Amount
 
-	LastBid, LastAsk *utils.DatedMetric
-	AvgBid, AvgAsk   utils.Amount
-	MaxBid, MaxAsk   *utils.DatedMetric
-	MinBid, MinAsk   *utils.DatedMetric
+	MaxBid, MaxAsk *utils.DatedMetric
+	MinBid, MinAsk *utils.DatedMetric
 }
 
-// GetUnderlying returns an asset's embedded Quote type.
-func (a Asset) GetUnderlying() Financial {
-	return a.Quote
-}
-
-// NewAsset instantiaties a new struct of type Asset.
-func NewAsset(q *Quote) *Asset {
+// NewAssetSumm instantiaties a new struct of type AssetSumm.
+func NewAssetSumm(q *Quote) *AssetSumm {
 	assetBid := &utils.DatedMetric{Amount: q.Bid, Date: q.Timestamp}
 	assetAsk := &utils.DatedMetric{Amount: q.Ask, Date: q.Timestamp}
 
-	return &Asset{
-		Quote: q,
-
+	return &AssetSumm{
+		nTicks: 1, TotalVolume: q.Volume,
 		AvgBid: assetBid.Amount, AvgAsk: assetAsk.Amount,
-		LastBid: assetBid, MaxBid: assetBid, MinBid: assetBid,
-		LastAsk: assetAsk, MaxAsk: assetAsk, MinAsk: assetAsk,
+		MaxBid: assetBid, MaxAsk: assetAsk,
+		MinBid: assetBid, MinAsk: assetAsk,
 	}
 }
 
-func (a Asset) Update(q Quote) error {
-	return a.update(q)
-}
-
-// Update uses new tick data to update an asset's metrics.
-func (a *Asset) update(q Quote) error {
+func (a *AssetSumm) Update(q Quote) {
 	// update bid metrics
-	a.AvgBid = utils.Avg(a.AvgBid, a.Nticks, q.Bid)
-	a.LastBid = &utils.DatedMetric{Amount: q.Bid, Date: q.Timestamp}
+	a.AvgBid = utils.Avg(a.AvgBid, a.nTicks, q.Bid)
 	a.MaxBid = utils.Max(a.MaxBid, q.Bid, q.Timestamp)
 	a.MinBid = utils.Min(a.MinBid, q.Bid, q.Timestamp)
 
 	// update ask metrics
-	a.AvgAsk = utils.Avg(a.AvgAsk, a.Nticks, q.Ask)
-	a.LastAsk = &utils.DatedMetric{Amount: q.Ask, Date: q.Timestamp}
+	a.AvgAsk = utils.Avg(a.AvgAsk, a.nTicks, q.Ask)
 	a.MaxAsk = utils.Max(a.MaxAsk, q.Ask, q.Timestamp)
 	a.MinAsk = utils.Min(a.MinAsk, q.Ask, q.Timestamp)
 
-	a.Nticks++
-	return nil
+	a.nTicks++
 }
 
 // ------------------------------------------------------------------
 
 // Holding structs refer the holding of a financial asset.
 type Holding struct {
-	Instrument
-	BuyPrice, SellPrice *utils.DatedMetric
+	*Quote
+	nSeen uint
 }
 
 // NewHolding instantities struct of type Holding.
-func NewHolding(i Instrument, buyPrice *utils.DatedMetric) *Holding {
+// Map Buy -> Bid, Sell -> Ask
+// Only open holdings/positions are allowed to be this type.
+// When partially/all sold off, becomes a struct.
+func NewHolding(q *Quote) *Holding {
 	return &Holding{
-		Instrument: i,
-		BuyPrice:   buyPrice,
+		Quote: q,
+		nSeen: 0,
 	}
 }
 
-// GetUnderlying method of Holding returns an instrument.Instrument type.
-func (h Holding) GetUnderlying() Financial {
-	return h.Instrument
+// SellOff a holding, return a new Security; send signal to remove if volume of 0.
+func (h *Holding) SellOff(v utils.Amount, summ *AssetSumm, sellData *utils.DatedMetric) (*Security, bool) {
+	var signal bool
+	var security = NewSecurity(sellData, h, summ)
+
+	summ.TotalVolume -= v
+	h.Volume -= v
+	if h.Volume == 0 {
+		signal = true
+	}
+	signal = false
+
+	return security, signal
 }
 
 // ------------------------------------------------------------------
@@ -183,21 +130,61 @@ func (h Holding) GetUnderlying() Financial {
 // life of the financial asset in a trading environment. Because a Security struct
 // holds aggregate information regarding a financial asset, it is embedded into an Index or Benchmark.
 type Security struct {
-	Asset
-	BuyPrice, SellPrice *utils.DatedMetric
+	Holding
+	AssetSumm
+	SellDate time.Time
 }
 
 // NewSecurity instantiates a security object from Tick data.
-func NewSecurity(buy, sell *utils.DatedMetric, asset Asset) *Security {
-	return &Security{
-		Asset:    asset,
-		BuyPrice: buy, SellPrice: sell,
+func NewSecurity(sellAt *utils.DatedMetric, h *Holding, summ *AssetSumm) *Security {
+	s := &Security{
+		Holding:   *h,
+		AssetSumm: *summ,
+		SellDate:  sellAt.Date,
 	}
-}
+	s.Ask = sellAt.Amount
+	s.nTicks = s.nTicks - h.nSeen
 
-// GetUnderlying method for security returns an instrument.Asset type.
-func (s Security) GetUnderlying() Financial {
-	return s.Asset
+	return s
 }
 
 // ------------------------------------------------------------------
+
+// Order type represents the transaction of a holding
+type Order struct {
+	*Quote
+	Buy    bool // it's either a buy or sell
+	Status Status
+	Logic  Logic
+}
+
+// NewOrder returns a new order that will execute at nearest price.
+func NewOrder(buy bool, q *Quote) *Order {
+	return &Order{
+		Quote:  q,
+		Buy:    buy,
+		Status: open,
+		Logic:  market, // TEMP: for now, only accepting market orders
+	}
+}
+
+// Status variables refer to a status of an order's execution.
+type Status int
+
+const (
+	open Status = iota // 0
+	closed
+	cancelled
+	expired // 3
+)
+
+// Logic is used to identify when the order should be executed.
+type Logic int
+
+const (
+	market Logic = iota // 0
+	limit
+	stopLimit
+	stopLoss
+	dayTrade // 4
+)
